509f92e6727e2741a546af2f5e37be1dea363d56
Pamela <am.ap.el@hotmail.com>
2017-01-19 18:59:34 -0600

    regresando a django

--- myblog/urls.py
+++ myblog/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    
+
     url(r'^$', views.post_list),
+    #url(r'^admin/myblog', include(admin.site.urls)),
+	#url(r'', include('myblog.urls')),
     
 ]
